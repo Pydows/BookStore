e544e710114f8507cd62cad1a6838292851bab56
AFltn <florentin.axelle@gmail.com>
2021-05-17 15:51:55 +0200

    Quizz 6 fini + debut quizz 7

--- src/main/java/jframe/GUIQuizz6.java
+++ src/main/java/jframe/GUIQuizz6.java
@@ -8,10 +8,13 @@ import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 import javax.swing.JRadioButton;
 import java.awt.Font;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 
 public class GUIQuizz6 {
 
 	private JFrame frame;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -47,29 +50,36 @@ public class GUIQuizz6 {
 		
 		JLabel lblNewLabel = new JLabel("Tu pref\u00E8res une histoire :");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(0, 0, 436, 13);
+		lblNewLabel.setBounds(0, 10, 436, 29);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Courte et assez rapide \u00E0 lire");
+		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		rdbtnNewRadioButton.setBounds(6, 37, 203, 23);
+		rdbtnNewRadioButton.setBounds(16, 52, 203, 29);
 		frame.getContentPane().add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Plut\u00F4t longue qui te tiendra sur de nombreuses pages");
+		buttonGroup.add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		rdbtnNewRadioButton_1.setBounds(211, 38, 203, 21);
+		rdbtnNewRadioButton_1.setBounds(16, 83, 410, 44);
 		frame.getContentPane().add(rdbtnNewRadioButton_1);
 		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Style saga, qui t'accompagnera sur plusieurs tomes");
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Style saga, qui t'accompagnera sur 3-5 tomes");
+		buttonGroup.add(rdbtnNewRadioButton_2);
 		rdbtnNewRadioButton_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		rdbtnNewRadioButton_2.setBounds(6, 83, 203, 21);
+		rdbtnNewRadioButton_2.setBounds(16, 129, 410, 44);
 		frame.getContentPane().add(rdbtnNewRadioButton_2);
 		
 		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Que tu pourras suivre sur de nombreux tomes et de nombreuses ann\u00E9es");
+		buttonGroup.add(rdbtnNewRadioButton_3);
 		rdbtnNewRadioButton_3.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		rdbtnNewRadioButton_3.setBounds(211, 83, 203, 23);
+		rdbtnNewRadioButton_3.setBounds(16, 175, 410, 44);
 		frame.getContentPane().add(rdbtnNewRadioButton_3);
+		
+		JButton btnNewButton = new JButton("Suivant");
+		btnNewButton.setBounds(175, 232, 85, 21);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
