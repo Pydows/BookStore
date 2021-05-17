3750aa47995b2ea3109e5d104bce249467634736
Frédéric <f.blosse@wanadoo.fr>
2021-05-17 12:13:24 +0200

    Add 5 Quizz

--- src/main/java/jframe/GUIQuizz1.java
+++ src/main/java/jframe/GUIQuizz1.java
@@ -10,13 +10,14 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ButtonGroup;
+import java.awt.Toolkit;
 
 public class GUIQuizz1 {
 
-	private JFrame frame;
+	private JFrame frameQ1;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	
-	
+
+
 	/**
 	 * Launch the application.
 	 */
@@ -25,7 +26,7 @@ public class GUIQuizz1 {
 			public void run() {
 				try {
 					GUIQuizz1 window = new GUIQuizz1();
-					window.frame.setVisible(true);
+					window.frameQ1.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,60 +46,63 @@ public class GUIQuizz1 {
 	 */
 	private void initialize() {
 		// Définissez le frame
-		frame = new JFrame("Quel livre est fait pour vous ? (1/6) ");
-		frame.setBounds(50, 50, 750, 271);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
+		frameQ1 = new JFrame("Quel livre est fait pour vous ? (1/6) ");
+		frameQ1.setIconImage(Toolkit.getDefaultToolkit().getImage(GUIQuizz1.class.getResource("/image/livre.png")));
+		frameQ1.setTitle("Quel livre est fait pour vous ? (1/10) ");
+		frameQ1.setBounds(50, 50, 750, 271);
+		frameQ1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameQ1.getContentPane().setLayout(null);
+
 		JLabel lblNewLabel = new JLabel("Vous voulez une recommandations pour quel type de livre ?");
 		lblNewLabel.setBounds(26, 33, 355, 32);
-		frame.getContentPane().add(lblNewLabel);
-		
+		frameQ1.getContentPane().add(lblNewLabel);
+
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Roman");
 		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setBounds(54, 86, 109, 23);
-		frame.getContentPane().add(rdbtnNewRadioButton);
+		frameQ1.getContentPane().add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setActionCommand("roman");
-		
+
 		JRadioButton rdbtnManga = new JRadioButton("Manga");
 		buttonGroup.add(rdbtnManga);
 		rdbtnManga.setBounds(54, 133, 109, 23);
-		frame.getContentPane().add(rdbtnManga);
+		frameQ1.getContentPane().add(rdbtnManga);
 		rdbtnManga.setActionCommand("manga");
-		
+
 		JRadioButton rdbtnBd = new JRadioButton("BD");
 		buttonGroup.add(rdbtnBd);
 		rdbtnBd.setBounds(214, 86, 109, 23);
-		frame.getContentPane().add(rdbtnBd);
+		frameQ1.getContentPane().add(rdbtnBd);
 		rdbtnBd.setActionCommand("bd");
-		
+
 		JRadioButton rdbtnRecueilDePosie = new JRadioButton("Recueil de poesie");
 		buttonGroup.add(rdbtnRecueilDePosie);
 		rdbtnRecueilDePosie.setBounds(214, 133, 143, 23);
-		frame.getContentPane().add(rdbtnRecueilDePosie);
+		frameQ1.getContentPane().add(rdbtnRecueilDePosie);
 		rdbtnRecueilDePosie.setActionCommand("recueil de poesie");
-		
+
 		JRadioButton rdbtnPieceDeTheatre = new JRadioButton("piece de theatre");
 		buttonGroup.add(rdbtnPieceDeTheatre);
 		rdbtnPieceDeTheatre.setBounds(428, 106, 155, 23);
-		frame.getContentPane().add(rdbtnPieceDeTheatre);
+		frameQ1.getContentPane().add(rdbtnPieceDeTheatre);
 		rdbtnPieceDeTheatre.setActionCommand("piece de theatre");
-		
-		
+
+
 		JButton btnNewButton = new JButton("Valider");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getActionCommand().equals("Valider")) {
-					System.out.println("vous avez choisi comme type de livre : " + 
-							buttonGroup.getSelection().getActionCommand());
-//					frame.dispose();
-//					GUIQuizz2.main(null);
+					String Q1 = buttonGroup.getSelection().getActionCommand();
+					System.out.println("Vous voulez une recommandation pour un " + 
+							Q1);
+					frameQ1.dispose();
+					GUIQuizz2.main(null);
 				}
 			}
 		});
-		
+
 		btnNewButton.setBounds(245, 176, 155, 23);
-		frame.getContentPane().add(btnNewButton);
-		
+		frameQ1.getContentPane().add(btnNewButton);
+
 	}
 }
---
+++ src/main/java/jframe/GUIQuizz4.java
@@ -0,0 +1,112 @@
+package jframe;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+
+public class GUIQuizz4 {
+
+	private JFrame frameQ4;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIQuizz4 window = new GUIQuizz4();
+					window.frameQ4.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUIQuizz4() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		// Définissez le frame
+				frameQ4 = new JFrame("Quel livre est fait pour vous ? (4/10) ");
+				frameQ4.setIconImage(Toolkit.getDefaultToolkit().getImage(GUIQuizz3.class.getResource("/image/livre.png")));
+				frameQ4.setTitle("Quel livre est fait pour vous ? (3/10) ");
+				frameQ4.setBounds(50, 50, 462, 223);
+				frameQ4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frameQ4.getContentPane().setLayout(null);
+
+				JLabel lblNewLabel = new JLabel("Quel programme télé vous plaît le plus ? ");
+				lblNewLabel.setBounds(103, 11, 355, 32);
+				frameQ4.getContentPane().add(lblNewLabel);
+
+				JRadioButton rdbtnchtis = new JRadioButton("Les Marseillais et le CHt'is");
+				buttonGroup.add(rdbtnchtis);
+				rdbtnchtis.setBounds(10, 37, 456, 23);
+				frameQ4.getContentPane().add(rdbtnchtis);
+				rdbtnchtis.setActionCommand("marseillais");
+
+				JRadioButton rdbtnamourpre = new JRadioButton("L'amour est dans le pre");
+				buttonGroup.add(rdbtnamourpre);
+				rdbtnamourpre.setBounds(10, 63, 355, 23);
+				frameQ4.getContentPane().add(rdbtnamourpre);
+				rdbtnamourpre.setActionCommand("l'amour est dans le pre");
+
+				JRadioButton rdbtnenquete = new JRadioButton("Enquetes criminelles");
+				buttonGroup.add(rdbtnenquete);
+				rdbtnenquete.setBounds(10, 89, 355, 23);
+				frameQ4.getContentPane().add(rdbtnenquete);
+				rdbtnenquete.setActionCommand("Enquetes criminelles");
+
+				JRadioButton rdbtnhistoire = new JRadioButton("Secrets d'histoire");
+				buttonGroup.add(rdbtnhistoire);
+				rdbtnhistoire.setBounds(10, 115, 359, 23);
+				frameQ4.getContentPane().add(rdbtnhistoire);
+				rdbtnhistoire.setActionCommand("Secrets d'histoire");
+
+
+				JButton btnNewButton = new JButton("Valider");
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (e.getActionCommand().equals("Valider")) {
+							String Q4 = buttonGroup.getSelection().getActionCommand();
+							if(Q4 == "marseillais") {
+								System.out.println("Tu aimes vraiment les "+ Q4 + " ?!? No Jugement'");
+							}
+								
+							else {
+								System.out.println("Vous préferez " + Q4);
+							}
+							
+							frameQ4.dispose();
+							GUIQuizz5.main(null);
+						}
+					}
+				});
+
+				btnNewButton.setBounds(10, 150, 355, 23);
+				frameQ4.getContentPane().add(btnNewButton);
+
+			}
+}
